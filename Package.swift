cd4f41e2c65640d76e479e9d9e573cb7a6c5c10a
Rodrigo <rodrigo@rxdx.com.br>
2024-12-11 21:16:54 -0300

    Upgrade unit tests from XCTest to Swift Testing (#6)
    * Upgrade unit tests from XCTest to Swift Testing
    
    * Swift Testing CLI and GitHub Action
    
    * GitHub Action Setup Swift
    
    * GitHub Action Setup Swift 6.0.2
    
    * GitHub Action Setup Swift
    
    ---------
    
    Co-authored-by: Rodrigo Dumont <rodrigo@Rodrigos-MacBook-Pro.local>

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-testing.git", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,6 +25,9 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "RestApiTests",
-            dependencies: ["RestApi"]),
+            dependencies: [
+                "RestApi",
+                .product(name: "Testing", package: "swift-testing")
+            ]),
     ]
 )
